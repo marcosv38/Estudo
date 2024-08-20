14e6873fd679d301f669d494b8656fa17a07fb3c
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-20 17:52:36 -0300

    inserção do while para percorrer a lista de butões

--- Javascript/aluramidi-JS/main.js
+++ Javascript/aluramidi-JS/main.js
@@ -1,6 +1,12 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomPom() {
-  
+function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
+}
+
+let contador = 0;
+
+while(contador<listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador++;
 }
