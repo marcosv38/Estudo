2dd47a38e4fb7853db5ac84f9794c6b24487af00
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-20 19:53:02 -0300

    implementação da mudança de imagem ao apertar os botões

--- Javascript/Fokus-JS/script.js
+++ Javascript/Fokus-JS/script.js
@@ -3,18 +3,22 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
 const botaoIniciar = document.querySelector('.app__card-primary-button')
 
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src','/imagens/foco.png');
 })
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src','/imagens/descanso-curto.png');
 })
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src','/imagens/descanso-longo.png');
 })
