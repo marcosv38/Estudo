02d0ff5f691fefc597da6e8436a7af7b0b61d20b
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-20 18:24:02 -0300

    todas as teclas apertadas estão colocando efeito de botão ativo

--- Javascript/aluramidi-JS/main.js
+++ Javascript/aluramidi-JS/main.js
@@ -28,6 +28,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+        
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 
 }
