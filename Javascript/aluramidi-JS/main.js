ee9f16847f94d3ba284809aeabc843fb783da8d1
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-20 18:36:13 -0300

    colocando condição para apenas as teclas enter e espaço mudar a cor dos botões ao ativar

--- Javascript/aluramidi-JS/main.js
+++ Javascript/aluramidi-JS/main.js
@@ -28,11 +28,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
-        
+
     }
 
-    tecla.onkeydown = function (){
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+
     }
 
     tecla.onkeyup = function () {
