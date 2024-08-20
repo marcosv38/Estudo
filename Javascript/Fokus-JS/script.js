9cdffd34af94d15f74d7adc7a507480d453196e9
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-20 20:23:24 -0300

    trocando efeito de botões quando clicados

--- Javascript/Fokus-JS/script.js
+++ Javascript/Fokus-JS/script.js
@@ -5,24 +5,34 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
 
 const botaoIniciar = document.querySelector('.app__card-primary-button')
 
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
+    
 })
 
 
 function alterarContexto(contexto) {
+
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active');
+    });
+
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
 
@@ -42,6 +52,6 @@ function alterarContexto(contexto) {
         default:
             break
     }
-
+    
 
 }
