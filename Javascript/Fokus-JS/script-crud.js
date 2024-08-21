c573951abd0d0920a37d5b96c4af6f612b4a48e4
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-21 08:34:21 -0300

    impleentação da função de tarefa ativa

--- Javascript/Fokus-JS/script-crud.js
+++ Javascript/Fokus-JS/script-crud.js
@@ -3,9 +3,12 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const botaoCancelar = document.querySelector('.app__form-footer__button--cancel');
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
+
 
 //ao recuperar a lista de tarefas é necessário transformar para array novamente o || []  serve para caso n tenha nd no localstorage
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let tarefaSelecionada = null;
 
 function atualizarTarefas(){
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -54,6 +57,21 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    li.onclick = () =>{
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active');
+            });
+        if(tarefaSelecionada==tarefa){
+            paragrafoDescricaoTarefa.textContent = '';
+            tarefaSelecionada = null;
+            return;
+        }
+        tarefaSelecionada = tarefa;
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+        li.classList.add('app__section-task-list-item-active');
+    }
 
     return li;
 
