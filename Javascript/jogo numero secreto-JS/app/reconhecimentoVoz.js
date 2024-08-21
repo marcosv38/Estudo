1da76a16173388f1ad58689bd62cfb6927df9ee5
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-21 12:49:42 -0300

    Speech recognition e resultado na tela

---
+++ Javascript/jogo numero secreto-JS/app/reconhecimentoVoz.js
@@ -0,0 +1,24 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API#speech_recognition
+
+const elementoChute = document.getElementById('chute');
+
+window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recongnition = new SpeechRecognition();
+recongnition.lang = 'pt-Br';
+recongnition.start();
+
+recongnition.addEventListener('result', onSpeak);
+
+function onSpeak(e){
+    chute = e.results[0][0].transcript;
+    exibeChute(chute);
+}
+
+function exibeChute(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse</div>
+    <span class="box">${chute}</span>
+    `;
+}
