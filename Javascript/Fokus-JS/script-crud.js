f64232099352a64b6f8aaaa25279602cfb699e5b
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-21 07:04:26 -0300

    fazendo o form aparecer

---
+++ Javascript/Fokus-JS/script-crud.js
@@ -0,0 +1,7 @@
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+
+
+btnAdicionarTarefa.addEventListener('click', ()=>{
+    formAdicionarTarefa.classList.toggle('hidden');
+});
