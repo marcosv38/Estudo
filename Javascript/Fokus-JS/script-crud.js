2bf979176130246e714ff8ff21ac09bafca32f16
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-21 07:16:30 -0300

    salvamento do value do form em localstorage

--- Javascript/Fokus-JS/script-crud.js
+++ Javascript/Fokus-JS/script-crud.js
@@ -1,7 +1,20 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
 
+const tarefas = [];
 
 btnAdicionarTarefa.addEventListener('click', ()=>{
     formAdicionarTarefa.classList.toggle('hidden');
 });
+
+formAdicionarTarefa.addEventListener('submit',(evento)=>{
+    evento.preventDefault();
+
+    const tarefa = {
+        descricao: textArea.value
+    }
+
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas',JSON.stringify(tarefas));
+})
