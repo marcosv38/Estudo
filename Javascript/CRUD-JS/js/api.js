908804cabb2233a134d42920f5d4e95b0a52d6e0
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-26 12:15:10 -0300

    Projeto de CRUD

---
+++ Javascript/CRUD-JS/js/api.js
@@ -0,0 +1,74 @@
+const URL_BASE = "http://localhost:3000"
+
+const api = {
+  async buscarPensamentos() {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos`)
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao salvar pensamento')
+      throw error
+    }
+  },
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos/${id}`)
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao editar pensamento')
+      throw error
+    }
+  },
+
+  async excluirPensamento(id) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
+        method: "DELETE"
+      })
+    }
+    catch {
+      alert('Erro ao excluir um pensamento')
+      throw error
+    }
+  }
+  
+}
+
+export default api
