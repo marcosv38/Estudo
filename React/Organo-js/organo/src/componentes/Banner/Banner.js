d7c48bfb86489cb52c1ae3e32a55613905823c7e
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-09-06 07:44:52 -0300

    insere o banner no header

---
+++ React/Organo-js/organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner (){
+    return(
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="banner principal da pagina do organo"/>
+        </header>
+    )
+}
+
+export default Banner;
