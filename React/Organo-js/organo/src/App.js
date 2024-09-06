5eb66b538aab0930f13ed1d99bdc37c0515298ac
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-09-06 09:30:31 -0300

    insere lista suspensa e botão de submit

--- React/Organo-js/organo/src/App.js
+++ React/Organo-js/organo/src/App.js
@@ -1,14 +1,11 @@
 import Banner from './componentes/Banner/Banner.js';
-import CampoTexto from './componentes/CampoTexto/index.js';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto label="Nome" placeholder="Insira seu Nome"/>
-      <CampoTexto label="Cargo" placeholder="Insira seu Cargo"/>
-      <CampoTexto label="Imagem" placeholder="Insira o endereço da imagem"/>
-      
+      <Formulario/>
     </div>
   );
 }
---
+++ React/Organo-js/organo/src/componentes/Formulario/index.js
@@ -0,0 +1,40 @@
+import Botao from '../Botao';
+import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import './Formulario.css';
+
+const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Mobile',
+        'Inovacao e Gestão'
+    ]
+
+const aoSalvar = (evento)=>{
+    evento.preventDefault();
+    console.log('Form foi submetido');
+}
+
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto obrigatorio="true" label="Nome" placeholder="Insira seu Nome" />
+                <CampoTexto label="Cargo" placeholder="Insira seu Cargo" />
+                <CampoTexto label="Imagem" placeholder="Insira o endereço da imagem" />
+                <ListaSuspensa required="true" itens={times} label="Time"/>
+                <Botao>
+                    Criar Card    
+                </Botao>
+            </form>
+        </section>
+    );
+
+}
+
+export default Formulario;
