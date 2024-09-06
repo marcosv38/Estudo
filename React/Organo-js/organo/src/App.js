f904e1c4fb75ffe2fcdd47bd8c5fc6102ff37a60
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-09-06 13:45:48 -0300

    Finalizando projeto

--- React/Organo-js/organo/src/App.js
+++ React/Organo-js/organo/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodapé';
 
 function App() {
 
@@ -49,7 +50,7 @@ const times = [
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
+
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -57,8 +58,13 @@ const times = [
     <div className="App">
       <Banner />
       <Formulario times={times.map(item => item.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time=><Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
-
+      {times.map(time=><Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria} 
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}/>)}
+      <Rodape />
     </div>
   );
 }
