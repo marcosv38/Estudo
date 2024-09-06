268a493a2b1c14f2142d4e3b30a31d602b2b3d0f
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-09-06 08:13:05 -0300

    insere os campos de texto do form

--- React/Organo-js/organo/src/App.js
+++ React/Organo-js/organo/src/App.js
@@ -1,9 +1,13 @@
 import Banner from './componentes/Banner/Banner.js';
+import CampoTexto from './componentes/CampoTexto/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <CampoTexto label="Nome" placeholder="Insira seu Nome"/>
+      <CampoTexto label="Cargo" placeholder="Insira seu Cargo"/>
+      <CampoTexto label="Imagem" placeholder="Insira o endereço da imagem"/>
       
     </div>
   );
