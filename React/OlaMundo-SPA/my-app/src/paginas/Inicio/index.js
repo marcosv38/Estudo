9eae3445d962b07ccef4ee24776c59c4185cf304
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-09-15 16:58:51 -0300

    Atualização do projeto

---
+++ React/OlaMundo-SPA/my-app/src/paginas/Inicio/index.js
@@ -0,0 +1,11 @@
+import Banner from "componentes/Banner";
+
+export default function Inicio() {
+    return (
+        <main>
+            <Banner />
+
+            <h1>Olá mundo!</h1>
+        </main>
+    )
+}
---
+++ React/OlaMundo-SPA/my-app/src/routes.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./componentes/Menu";
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
